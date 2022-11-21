6875706bef351e1dfe595fae0eee7bcc3f84e98c
Eric Serrat <eric.serrat@cevinio.com>
2022-11-21 20:59:49 +0100

    Move components to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
   const classes = 'card ' + props.className;
   return <div className={classes}>{props.children}</div>
 }
