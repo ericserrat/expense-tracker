4e57c131d02b5ac05f513f8cfa46e0a1f21e2c15
Eric Serrat <eric.serrat@cevinio.com>
2022-11-21 20:53:42 +0100

    Organize components

---
+++ src/components/UI/Card.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import './Card.css';
+
+function Card(props) {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>
+}
+
+Card.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.array,
+};
+
+export default Card;
