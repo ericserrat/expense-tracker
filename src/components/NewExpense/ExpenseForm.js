762ab1ca76886912aee7dd7654f38e141270ae04
Eric Serrat <eric.serrat@cevinio.com>
2022-11-23 10:12:58 +0100

    Add expense form submit handler (+ example of single state)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -11,8 +11,41 @@ const ExpenseForm = () => {
   const amountChangeHandler = event => setEnteredAmount(event.target.value);
   const dateChangeHandler = event => setEnteredDate(event.target.value);
 
+  /*
+  // Example with one single state
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+  const titleChangeHandler = (event) => {
+    // WRONG WAY
+    setUserInput({
+      ...userInput,
+      enteredTitle
+    });
+
+    // CORRECT WAY - Use previous state
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    })
+  };
+  */
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
