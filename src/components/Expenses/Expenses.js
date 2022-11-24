779e10aeb0ced088e0055b5b1deccad5e06b52cf
Eric Serrat <eric.serrat@cevinio.com>
2022-11-24 09:44:26 +0100

    Show no expenses found in list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,11 +14,18 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(e => e.date.getFullYear().toString() === filteredYear);
 
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(e =>
+      <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date}/>
+    );
+  }
+
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {filteredExpenses.map(e => <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date}/> )}
+        {expensesContent}
       </Card>
     </div>
   );
